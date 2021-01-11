4e8dc257b95844d3fa68518b5fd35c0657e3e3ce
Micael Carlstedt <micael@carlstedt.com>
2021-01-11 23:28:57 +0100

    Updates

---
+++ precache-manifest.f87c5aa1b89f7d5bfebaf71165eb9c2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ca8259eb2f30e9f449d5b95c25159d",
+    "url": "/shark-shop/index.html"
+  },
+  {
+    "revision": "cfe207706acf2c2a1296",
+    "url": "/shark-shop/static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "68ac6e4eb1af20f69aba",
+    "url": "/shark-shop/static/css/main.0d94d79d.chunk.css"
+  },
+  {
+    "revision": "cfe207706acf2c2a1296",
+    "url": "/shark-shop/static/js/2.14fe7388.chunk.js"
+  },
+  {
+    "revision": "c4381524cae000488e2fb889012466d0",
+    "url": "/shark-shop/static/js/2.14fe7388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ac6e4eb1af20f69aba",
+    "url": "/shark-shop/static/js/main.3948db83.chunk.js"
+  },
+  {
+    "revision": "461a6d9ebf912f3d5283",
+    "url": "/shark-shop/static/js/runtime-main.b9d15c81.js"
+  },
+  {
+    "revision": "b7c58c41ab15857bebc201b40aeb1c85",
+    "url": "/shark-shop/static/media/logo.b7c58c41.svg"
+  }
+]);
